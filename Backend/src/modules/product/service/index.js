e22fa01b138e0533c5ddee93336fac9c200f8acb
Mohammad Atif <mhdatif0511@gmail.com>
2025-11-26 21:15:29 +0530

    feat: Add user and vendor service modules; restructure product service exports

---
+++ Backend/src/modules/product/service/index.js
@@ -0,0 +1,4 @@
+export * from "./product.create.service.js"
+export * from "./product.delete.service.js"
+export * from "./product.get.service.js"
+export * from "./product.update.service.js"
---
+++ Backend/src/modules/product/service/product.create.service.js
@@ -0,0 +1,6 @@
+import { createProductSchema } from "../schema/index.js"
+import { ApiError } from "../../../utils/apiError.js"
+import { redisSetKey } from "../../../utils/redisHelper.js"
+import {Product} from "../../index.model.js"
+
+

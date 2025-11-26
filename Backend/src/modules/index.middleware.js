544c17300dd14b5eb3b9654809153d71c247b965
Mohammad Atif <mhdatif0511@gmail.com>
2025-11-26 20:40:38 +0530

    refactor: Remove unused index files for improved code clarity and maintainability

--- Backend/src/modules/index.middleware.js
+++
@@ -1,9 +0,0 @@
-import {} from './admin/middleware/index.js'
-import {} from './customer/middleware/index.js'
-import {} from './deliveryPerson/middleware/index.js'
-import {} from './fieldMan/middleware/index.js'
-import {} from './order/middleware/index.js'
-import {} from './product/middleware/index.js'
-import {} from './vendor/middleware/index.js'
-import {} from './user/middleware/index.js'
-
